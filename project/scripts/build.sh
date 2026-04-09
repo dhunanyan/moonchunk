e237c808a97085d0914e4ce7b048ee4e906f09ba
dhunanyan <davit.hunanyan@me.com>
2026-04-09 10:21:09 +0200

    fix: resolve build.sh issues by specified direct path in node_modules

--- project/scripts/build.sh
+++ project/scripts/build.sh
@@ -1,2 +1,5 @@
-antlr4ts -visitor -o .antlr ./MoonChunk.g4
-tsc -p tsconfig.json
+#!/usr/bin/env sh
+set -eu
+
+./node_modules/.bin/antlr4ts -visitor -o .antlr ./MoonChunk.g4
+./node_modules/.bin/tsc -p tsconfig.json
